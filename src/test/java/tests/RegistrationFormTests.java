f91a6371ddd4d8bc892dafd0d4f142f4b8f6968b
Igor Zvyagintsev <zvyagintsevis@yandex.ru>
2022-02-14 16:25:42 +0300

    add pajeobject homework

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -3,16 +3,26 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
 
-
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationFormTests {
+    RegistrationPage registrationPage = new RegistrationPage();
+    String firstName = "Igor";
+    String lastName = "Zvyagintsev";
+    String userEmail = "zvyagintsevis@yandex.ru";
+    String gender = "Male";
+    String mobile = "1234567890";
+    String day = "13";
+    String year = "1993";
+    String month = "May";
+    String subjects = "Commerce";
+    String hobbySport = "Sport";
+    String hobbyMusic = "Music";
+    String picture = "nastroenie0007.jpg";
+    String currentAddress = "Moscow";
+    String state = "Haryana";
+    String city = "Panipat";
 
     @BeforeAll
     static void beforeAll() {
@@ -22,41 +32,29 @@ public class RegistrationFormTests {
 
     @Test
     void successFillTest() {
-        open("/automation-practice-form");
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(userEmail)
+                .setGender(gender)
+                .setMobile(mobile)
+                .setDate(day, month, year)
+                .setSubjects(subjects)
+                .setHobbiesSports()
+                .setHobbiesMusic()
+                .setPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .clickSubmit();
 
-        $("#firstName").setValue("Igor");
-        $("#lastName").setValue("Zvyagintsev");
-        $("#userEmail").setValue("zvyagintsevis@yandex.ru");
-        $(byText("Male")).click();
-        $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOption("1993");
-        $(".react-datepicker__month-select").selectOption("May");
-        $(byText("13")).click();
-        $("#subjectsInput").setValue("Commerce").pressEnter();
-        $(byText("Sports")).click();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("nastroenie0007.jpg");
-        $("#currentAddress").setValue("Moscow");
-        $("#state").scrollTo().click();
-        $(byText("Haryana")).click();
-        $("#city").scrollTo().click();
-        $(byText("Panipat")).click();
-        $("#submit").click();
 
-        // проверки заполнения полей
-
-        $(".table-responsive").shouldHave(
-                text("Igor Zvyagintsev"),
-                text("zvyagintsevis@yandex.ru"),
-                text("Male"),
-                text("1234567890"),
-                text("13 May,1993"),
-                text("Commerce"),
-                text("Sports"),
-                text("Music"),
-                text("nastroenie0007.jpg"),
-                text("Moscow"),
-                text("Haryana Panipat"));
+        // проверка заполнения полей
+        registrationPage.checkForm(
+                firstName, lastName, userEmail, gender, mobile,
+                day, month, year, subjects, hobbySport, hobbyMusic,
+                picture, currentAddress, state, city);
     }
+
 }
+
